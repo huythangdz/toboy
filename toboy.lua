277cdc7f8dfc399efdd18ca235ac7660ea3b6f54
huythangdz <hahahnguyenhuythangdz@gmail.com>
2025-04-21 11:33:35 +0700

    Add files via upload

--- toboy.lua
+++ toboy.lua
@@ -33,7 +33,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
      local Tab1o = MakeTab({Name = "Script Farm"})
      local Tab2o = MakeTab({Name = "Script kaitun"})
     local Tab3o = MakeTab({Name = "Script fram rương"})
- local Tab4o = MakeTab ({Name = "script Fix lag" }) AddButton(Tab2o, {
+  AddButton(Tab2o, {
   Name = "Quartz Kaitun Hub",
  Callback = function()getgenv().Mode = "OneClick"
   getgenv().Setting = {
@@ -77,101 +77,9 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/r
   end 
   }) AddButton(Tab3o, {
     Name = "CHEST VGB",
-    Callback = function()loadstring(game:HttpGet("https://raw.githubusercontent.com/VGB-VGB-VGB/-VGB-Chest-Farm--/refs/heads/main/ChestFarmByVGBTeam"))() end }) AddButton(Tab4o, {
-      Name = "Fix lag",
-      Callback = function()local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/Chienmythic/MakeMenu/refs/heads/main/vtiosmake"))()
- 
-        local PhantomForcesWindow = Library:NewWindow("Chienmythic")
-         
-        local KillingCheats = PhantomForcesWindow:NewSection("Main Antilag")
-         
-        KillingCheats:CreateButton("Antilag", function()
-          loadstring(game:HttpGet("https://raw.githubusercontent.com/Chienmythic/MakeMenu/refs/heads/main/mainv1.lua.txt"))()
-        end)
-         
-        KillingCheats:CreateButton("Antilag", function()
-          local ToDisable = {
-          Textures = true,
-          VisualEffects = true,
-          Parts = true,
-          Particles = true,
-          Sky = true
-        }
-         
-        local ToEnable = {
-          FullBright = false
-        }
-         
-        local Stuff = {}
-         
-        for _, v in next, game:GetDescendants() do
-          if ToDisable.Parts then
-            if v:IsA("Part") or v:IsA("Union") or v:IsA("BasePart") then
-              v.Material = Enum.Material.SmoothPlastic
-              table.insert(Stuff, 1, v)
-            end
-          end
-         
-          if ToDisable.Particles then
-            if v:IsA("ParticleEmitter") or v:IsA("Smoke") or v:IsA("Explosion") or v:IsA("Sparkles") or v:IsA("Fire") then
-              v.Enabled = false
-              table.insert(Stuff, 1, v)
-            end
-          end
-         
-          if ToDisable.VisualEffects then
-            if v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("DepthOfFieldEffect") or v:IsA("SunRaysEffect") then
-              v.Enabled = false
-              table.insert(Stuff, 1, v)
-            end
-          end
-         
-          if ToDisable.Textures then
-            if v:IsA("Decal") or v:IsA("Texture") then
-              v.Texture = ""
-              table.insert(Stuff, 1, v)
-            end
-          end
-         
-          if ToDisable.Sky then
-            if v:IsA("Sky") then
-              v.Parent = nil
-              table.insert(Stuff, 1, v)
-            end
-          end
-        end
-         
-        game:GetService("TestService"):Message("Effects Disabler Script : Successfully disabled "..#Stuff.." assets / effects. Settings :")
-         
-        for i, v in next, ToDisable do
-          print(tostring(i)..": "..tostring(v))
-        end
-         
-        if ToEnable.FullBright then
-            local Lighting = game:GetService("Lighting")
-         
-            Lighting.FogColor = Color3.fromRGB(255, 255, 255)
-            Lighting.FogEnd = math.huge
-            Lighting.FogStart = math.huge
-            Lighting.Ambient = Color3.fromRGB(255, 255, 255)
-            Lighting.Brightness = 5
-            Lighting.ColorShift_Bottom = Color3.fromRGB(255, 255, 255)
-            Lighting.ColorShift_Top = Color3.fromRGB(255, 255, 255)
-            Lighting.OutdoorAmbient = Color3.fromRGB(255, 255, 255)
-            Lighting.Outlines = true
-        end
-        end)
-        
-        local KillingCheats = PhantomForcesWindow:NewSection("Main Unlock FPS")
-         
-        KillingCheats:CreateButton("Unlock Fps", function()
-          while true do
-            local args = {
-                [1] = 1000
-            }
-            game:GetService("ReplicatedStorage").UpdateFPS:FireServer(unpack(args))
-            wait(0.01)
-        end }) AddButton(Tab3o, {
+    Callback = function()loadstring(game:HttpGet("https://raw.githubusercontent.com/VGB-VGB-VGB/-VGB-Chest-Farm--/refs/heads/main/ChestFarmByVGBTeam"))() end }) 
+     
+ AddButton(Tab3o, {
           Name = "Fram cheat",
           Callback = function()_G.Delay = 3 -- 1 : 1 sec
             _G.ChestType = "Chest"
